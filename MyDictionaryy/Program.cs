190cd18f0c69599ab726c24bd31d2280552a9849
ali akbaş <123akbaa@gmail.com>
2021-07-05 14:08:14 +0300

    Add project files.

---
+++ MyDictionaryy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DemoMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> array = new Dictionary<string, int>();
+            array.Add("ali", 250);
+            array.Add("veli", 350);
+            array.Add("ahmet", 450);
+            array.Add("salih", 550);
+
+            Console.WriteLine(array.Count);
+        }
+    }
+}
